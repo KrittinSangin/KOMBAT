f4fa4a629fffe27ab483438d14fc0918082e1462
kanthidaja <kanthida_khun@cmu..ac.th>
2026-03-01 16:28:49 +0700

    render the number of minionbar depeneds on minionCount from config page

--- kombat-frontend/components/Navbar.tsx
+++ kombat-frontend/components/Navbar.tsx
@@ -16,12 +16,24 @@ export default function Navbar({title,minionCount,team}: NavbarProps){
         <>
             <div className="z-20 absolute w-[1469px] h-[80px] bg-[#A9B6FF] top-0 left-0 flex items-center justify-between px-6">
                 <h1 className="text-[50px] font-bold">{title}</h1>
-            {Array.from({ length: minionCount }).map((_, index) => (
-            <button key={index}>
-             <Image src={team === TeamSide.Blue ? "/blue_btn.PNG" : "/red_btn.PNG"} alt="Minion" width={80} height={40}   />
-              </button>
-            ))}
-            </div>
+            
+            {/* กลาง */}
+      <div className="absolute left-[610px] bottom-[-5px] w-2/3 flex justify-center gap-5">
+        {Array.from({ length: minionCount }).map((_, index) => (
+          <button key={index}>
+            <Image
+              src={team === TeamSide.Blue ? "/blue_btn.PNG" : "/red_btn.PNG"}
+              alt="Minion"
+              width={120}
+              height={50}
+            />
+          </button>
+        ))}
+      </div>
+
+      <div className="w-1/3"></div>
+
+      </div>
         </>
     )
 }
